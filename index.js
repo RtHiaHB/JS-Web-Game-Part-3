e5b7f3633d5e54326fbb920fab310872794c5aa1
Thomas Davidson <tomd1969@gmail.com>
2022-10-06 21:47:23 -0400

    Final commit for this part.

--- index.js
+++ index.js
@@ -1,23 +1,31 @@
-function newImage(url, left, bottom){
+function newImage(url){
     let image = document.createElement('img')
     image.src = url
-    image.style.position = 'fixed'
-    image.style.left = left + 'px'
-    image.style.bottom = bottom + 'px'
     document.body.append(image)
     return image
 }
 
-newImage('assets/green-character.gif', 100, 250)
-newImage('assets/tree.png', 200, 450)
-newImage('assets/pillar.png', 350, 250)
-newImage('assets/pine-tree.png', 450, 350)
-newImage('assets/crate.png', 150, 350)
-newImage('assets/well.png', 500, 575)
+move(newImage('assets/green-character.gif')).to(100,250);
+move(newImage('assets/tree.png')).to(200, 450);
+move(newImage('assets/pillar.png')).to(350, 250);
+move(newImage('assets/pine-tree.png')).to(450, 350);
+move(newImage('assets/crate.png')).to(150, 350);
+move(newImage('assets/well.png')).to(500, 575);
 
+function move(image) {
+    image.style.position = 'fixed';
 
+    function moveToCoordinates(left, bottom) {
+        image.style.left = left + 'px';
+        image.style.bottom = bottom + 'px';
+    }
+    return {
+        to: moveToCoordinates
+    }
+}
 function newItem(url, left, bottom){
-    let item = newImage(url, left, bottom)
+    let item = newImage(url);
+    move(item).to(left, bottom);
     item.addEventListener('click', () => {
         item.remove()
         let inventoryItem = document.createElement('img')
